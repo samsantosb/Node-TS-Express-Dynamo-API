096769cfedb9827f55ed233e2ba7a4bbaf6ba92d
samsantosb <samsantosb@outlook.com>
2022-11-20 21:33:37 -0300

    controller: done and start to write some mocks

--- src/medicines/__mocks__/fake.medicine.service.ts
+++ src/medicines/__mocks__/fake.medicine.service.ts
@@ -1,1 +1,8 @@
-export const fakeMedicineService = {};
+import { IMedicineRepository } from "./../repositories/medicine.repository.interface";
+import { IMedicineService } from "./../services/medicine.service.interface";
+export class FakeMedicineService implements IMedicineService {
+  constructor(private readonly medicineRepository: IMedicineRepository) {}
+  async getAll(): Promise<any> {
+    return [];
+  }
+}
--- src/medicines/controllers/medicine.controller.ts
+++ src/medicines/controllers/medicine.controller.ts
@@ -1,8 +1,10 @@
 import { IMedicineController } from "./medicine.controller.interface";
 import { IMedicineService } from "./../services/medicine.service.interface";
 import { StatusCode } from "../../utils/statusCode/status.code";
+import { Request, Response } from "express";
 export class MedicineController implements IMedicineController {
   constructor(private readonly medicineService: IMedicineService) {}
+
   async getAll(req: Request, res: Response): Promise<void> {
     const medcines = await this.medicineService.getAll();
     if ("promiseError" in medcines) {
@@ -10,5 +12,41 @@ export class MedicineController implements IMedicineController {
       return;
     }
     res.status(StatusCode.OK).json(medcines);
+  }
+
+  async getById(req: Request, res: Response): Promise<void> {
+    const medicine = await this.medicineService.getById(req.params.id);
+    if ("promiseError" in medicine) {
+      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(medicine);
+      return;
+    }
+    res.status(StatusCode.OK).json(medicine);
+  }
+
+  async create(req: Request, res: Response): Promise<void> {
+    const medicine = await this.medicineService.create(req.body);
+    if ("promiseError" in medicine) {
+      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(medicine);
+      return;
+    }
+    res.status(StatusCode.CREATED).json(medicine);
+  }
+
+  async update(req: Request, res: Response): Promise<void> {
+    const medicine = await this.medicineService.update(req.body);
+    if ("promiseError" in medicine) {
+      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(medicine);
+      return;
+    }
+    res.status(StatusCode.OK).json(medicine);
+  }
+
+  async delete(req: Request, res: Response): Promise<void> {
+    const medicine = await this.medicineService.delete(req.params.id);
+    if (typeof medicine !== "string" && "promiseError" in medicine) {
+      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(medicine);
+      return;
+    }
+    res.status(StatusCode.OK).json(medicine);
   }
 }
