56ad82fad4eed07f27acde8de088be3c9dd71423
Amanda Soares <amandchen@hotmail.com>
2022-11-20 22:02:56 -0300

    mocks: fake medicine repository created

--- src/medicines/__mocks__/fake.medicine.repository.ts
+++ src/medicines/__mocks__/fake.medicine.repository.ts
@@ -1,1 +1,26 @@
-export const fakeMedicineRepository = {};
+import { fakeMedicines, fakeMedicine } from './fake.medicine.data';
+import { IMedicineRepository } from './../repositories/medicine.repository.interface';
+import { IMedicine, IMedicineModel } from '../models/medicine.model.interface';
+
+export class FakeMedicineRepository implements IMedicineRepository {
+  constructor(private readonly medicineModel: IMedicineModel) {}
+  getAll(): Promise<IMedicine[]> {
+    return Promise.resolve(fakeMedicines);
+  }
+
+  getById(id: string): Promise<IMedicine> {
+    return Promise.resolve(fakeMedicine);
+  }
+
+  create(medicine: IMedicine): Promise<IMedicine> {
+    return Promise.resolve(fakeMedicine);
+  }
+
+  update(medicine: IMedicine): Promise<IMedicine> {
+    return Promise.resolve(fakeMedicine);
+  }
+
+  delete(id: string): Promise<string> {
+    return Promise.resolve(`Medicine with id ${id} deleted`);
+  }
+};
