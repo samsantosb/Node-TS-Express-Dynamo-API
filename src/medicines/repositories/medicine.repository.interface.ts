15ef11ac47cf57b715ebf39665f90248cd8d1dba
samsantosb <samsantosb@outlook.com>
2022-11-21 00:13:40 -0300

    Repository-abstraction: small correction on names

--- src/medicines/repositories/medicine.repository.interface.ts
+++ src/medicines/repositories/medicine.repository.interface.ts
@@ -1,9 +1,9 @@
-import { IMedicine } from "../models/medicine.model.interface";
+import { Medicine } from "../models/medicine.model";
 
 export interface IMedicineRepository {
-  getAll(): Promise<IMedicine[]>;
-  getById(id: string): Promise<IMedicine>;
-  create(medicine: IMedicine): Promise<IMedicine>;
-  update(medicine: IMedicine): Promise<IMedicine>;
+  getAll(): Promise<Medicine[]>;
+  getById(id: string): Promise<Medicine>;
+  create(medicine: Medicine): Promise<Medicine>;
+  update(medicine: Medicine): Promise<Medicine>;
   delete(id: string): Promise<string>;
 }
